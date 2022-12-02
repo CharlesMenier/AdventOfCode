45dd27bc5b5dd1cacbe0e9ceddf026dfe6ed9251
Charles Ménier <charles.menier@bedrockstreaming.com>
2022-12-02 15:15:16 +0100

    typo

--- src/2022/routes.ts
+++ src/2022/routes.ts
@@ -20,7 +20,7 @@ export default [
         Day2()
             .then(result => {
                 res.render('day', {
-                    title: 'Day 2: ock Paper Scissors',
+                    title: 'Day 2: Rock Paper Scissors',
                     question1: 'What would your total score be if everything goes exactly according to your strategy guide?',
                     question2: 'Following the Elf\'s instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?',
                     input1: result.solution1Input,
